311b98f5f22767f36d6999b4fe172a0d92c9aa90
MinDiIS <petr48142@gmail.com>
2024-12-26 13:27:51 +0400

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/GetgInput.cs
@@ -0,0 +1,16 @@
+﻿public static class GetgInput
+{
+    public static bool GetResponce()
+    {
+        string text = Console.ReadLine();
+        bool result = text switch
+        {
+            "да" => true,
+            "Да" => true,
+            "нет" => false,
+            "Нет" => false,
+            _ => true,
+        };
+        return result;
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,37 @@
+﻿string[] allQuest = new string[]
+{
+    "Ты любишь маму?",
+    "Ты гей",
+    "Ты натурал?",
+    "Ты любишь Диму",
+    "ты сделал лабу?",
+};
+
+while (true)
+{
+    Huy();
+    Console.ReadLine();
+}
+
+void Huy()
+{
+
+    Random r = new Random();
+    int index = r.Next(0, allQuest.Length - 1);
+
+    Console.WriteLine(allQuest[index]);
+
+    bool result = GetgInput.GetResponce();
+
+    Console.Clear();
+    if (result)
+    {
+        Console.WriteLine("Сосал?");
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+        Console.WriteLine("Пидора ответ");
+    }
+}
